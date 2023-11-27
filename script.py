97ec695b76368a39d429c85193de0daa443ede9c
Pieter Provoost <pieterprovoost@gmail.com>
2023-11-27 13:17:34 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,29 @@
+import requests
+import re
+from email.utils import format_datetime
+from dateutil import parser
+
+
+gbif_mgnify_page = "https://hosted-datasets.gbif.org/mgnify/"
+selected_datasets = ["MGYS00005294"]
+
+response = requests.get(gbif_mgnify_page)
+data = response.text
+
+p = re.compile(">(MGY.*).zip</a>.*>([0-9\-\s:]+)</td>")
+lines = re.findall(p, data)
+
+output = "<rss>"
+
+for line in lines:
+    if line[0] in selected_datasets:
+        (dataset_id, time) = line
+        d = parser.parse(time.strip().replace(" ", "T"))
+        pubdate = format_datetime(d)
+        output = output + f"<item><title>{dataset_id}</title><link>https://hosted-datasets.gbif.org/mgnify/{dataset_id}.zip</link><ipt:dwca>https://hosted-datasets.gbif.org/mgnify/{dataset_id}.zip</ipt:dwca><pubDate>{pubdate}</pubDate></item>"
+
+output = output + "</rss>"
+
+file = open("feed.rss", "w")
+file.write(output)
+file.close()
